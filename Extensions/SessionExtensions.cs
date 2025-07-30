e43ad72b0200a5e9a932b4709aef5cd94ea9fb1f
Luna Mendez Robles <luna.mendez.robles@tajamar365.com>
2025-07-30 17:29:44 +0200

    Views parte 1

--- Extensions/SessionExtensions.cs
+++ Extensions/SessionExtensions.cs
@@ -3,11 +3,11 @@ using Microsoft.AspNetCore.Http;
 
 namespace TiendaApp.Extensions
 {
-    public static class SessionExtensions
+    public static class SessionExtensions // Esta clase extiende la funcionalidad de la sesión para almacenar y recuperar objetos como JSON
     {
-        public static void SetObjectAsJson<T>(this ISession session, string key, T value)
+        public static void SetObjectAsJson<T>(this ISession session, string key, T value) // Método para almacenar un objeto en la sesión como JSON
         {
-            session.SetString(key, JsonSerializer.Serialize(value));
+            session.SetString(key, JsonSerializer.Serialize(value)); // Serializa el objeto a JSON y lo almacena en la sesión con la clave especificada
         }
 
         public static T? GetObjectFromJson<T>(this ISession session, string key)
